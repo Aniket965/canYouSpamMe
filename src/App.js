12dfe9710923acaff4da6cbc362f1896218657eb
aniket965 <aniket965.as@gmail.com>
2018-02-10 13:05:17 +0530

    Added UI

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: null,
+      isSpam: null,
+      isloaded: false
+    };
+    this.detector = this.detector.bind(this);
+  }
+  detector() {
+    this.setState({ isSpam: false, isloaded: true });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      {this.state.isloaded?(
+         <div className={`result ${this.state.isSpam ? "spam" : "notspam"}`}>
+         <h1> {this.state.isSpam? "Spam 🙁":"Not Spam 🤩! "} </h1>
+       </div>
+      ):null}
+
+        <div className={`content  ${this.state.isloaded?('fadeout'):""}`}>
+          <h1> Spam Detector </h1>
+          <div className="input">
+            <form
+              onSubmit={e => {
+                e.preventDefault();
+                this.detector();
+              }}
+            >
+              <input
+                className="input-search"
+                placeholder="Message..."
+                type="text"
+                onChange={value =>
+                  this.setState({ message: value.target.value })
+                }
+              />
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
